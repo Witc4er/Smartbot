e3ec1da7d1ae9995d9d51b91f91731e1129a6b16
Witc4er <85217128+Witc4er@users.noreply.github.com>
2021-08-27 19:51:32 +0300

    Changed file main.py

---
+++ app.py
@@ -0,0 +1,5 @@
+COMMAND = ['add_contact', 'delete_contact', 'change_contact', 'search_contact', 'show_contacts', 'show_birthday',
+           'add_note', 'delete_note', 'change_note', 'search_note', 'show_notes']
+
+
+def main()
