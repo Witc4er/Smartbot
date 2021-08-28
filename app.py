f107f44c26e92e3f53f7a7beb5d2fdcf2237dc88
Witc4er <85217128+Witc4er@users.noreply.github.com>
2021-08-28 16:55:05 +0300

    Changed function main

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-COMMAND = ['add_contact', 'delete_contact', 'change_contact', 'search_contact', 'show_contacts', 'show_birthday',
-           'add_note', 'delete_note', 'change_note', 'search_note', 'show_notes']
+
+
+def exit_handler():
+    return
+
+
+def unknown_cmd():
+    return "Unknown command"
+
+
+COMMAND = {'add_contact': '',
+           'delete_contact': '',
+           'change_contact': '',
+           'search_contact': '',
+           'show_contacts': '',
+           'show_birthday': '',
+           'add_note': '',
+           'delete_note': '',
+           'change_note': '',
+           'search_note': '',
+           'show_notes': '',
+           'exit': exit_handler}
 
 
 def main():
     while True:
         user_input = input('Input your command: ')
         command = user_input.lower()
-        if command in COMMAND:
-            handler = COMMAND.get(command.lower(), 'unknown_cmd')
+        handler = COMMAND.get(command.lower(), unknown_cmd)
+        result = handler()
+        if not result:
+            print('Bye!')
+            break
+        print(result)
 
 
 if __name__ == '__main__':
