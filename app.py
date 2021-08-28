df4ffe3404f3e51135bbcb42ef0bed53601a462a
Witc4er <85217128+Witc4er@users.noreply.github.com>
2021-08-28 16:37:13 +0300

    Changed app.py

--- app.py
+++ app.py
@@ -2,4 +2,13 @@ COMMAND = ['add_contact', 'delete_contact', 'change_contact', 'search_contact', 
            'add_note', 'delete_note', 'change_note', 'search_note', 'show_notes']
 
 
-def main()
+def main():
+    while True:
+        user_input = input('Input your command: ')
+        command = user_input.lower()
+        if command in COMMAND:
+            handler = COMMAND.get(command.lower(), 'unknown_cmd')
+
+
+if __name__ == '__main__':
+    main()
